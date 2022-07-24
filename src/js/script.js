a9564dc6850b63bc197748e4fd2ce05dd47039a1
Marcello Prado <marcellopradomuller@gmail.com>
2022-07-23 22:40:53 -0300

    Finalizando Validação e criando o reset.css

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,21 @@
 "use strict"
 
-let form = document.getElementById('form')
-let campos = document.querySelectorAll("input[name='teste']")
-// let small = document.querySelectorAll('.small')
-
+const form = document.getElementById('form')
+const campos = document.querySelectorAll('.inputs')
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
         
-    campos.forEach(function(inputs) {
+    campos.forEach(function(inputs, small) {
         if(inputs.value === '') {
 
             inputs.classList.add('erro')
+            inputs.nextElementSibling.classList.add("erro")
            
         } else {
             inputs.classList.remove('erro')
+            inputs.nextElementSibling.classList.remove("erro")
             inputs.classList.add('sucesso')
         }
 })
-
-
-});
+})
